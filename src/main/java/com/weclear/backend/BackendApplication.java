031ed492adb2ccddfb5f82f0a410b9570973b42b
Vinnu Polisetti <vinnup34@gmail.com>
2025-01-31 18:49:29 -0700

    trying to deploy

--- src/main/java/com/weclear/backend/BackendApplication.java
+++ src/main/java/com/weclear/backend/BackendApplication.java
@@ -3,8 +3,16 @@ package com.weclear.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class BackendApplication {
+
+	@PostConstruct
+	public void init() {
+		System.out.println("DB URL: " + System.getenv("SPRING_DATASOURCE_URL"));
+		System.out.println("DB Username: " + System.getenv("SPRING_DATASOURCE_USERNAME"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
