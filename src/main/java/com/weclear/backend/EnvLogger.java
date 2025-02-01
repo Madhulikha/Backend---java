e4b8ce79325f7dc2ee811595cb5265520f08f8a9
Vinnu Polisetti <vinnup34@gmail.com>
2025-01-31 20:29:35 -0700

    try 1

---
+++ src/main/java/com/weclear/backend/EnvLogger.java
@@ -0,0 +1,19 @@
+package com.weclear.backend;
+
+
+import org.springframework.stereotype.Component;
+
+import jakarta.annotation.PostConstruct;
+
+@Component
+public class EnvLogger {
+
+    @PostConstruct
+    public void logEnvVars() {
+        System.out.println("MYSQLHOST: " + System.getenv("MYSQLHOST"));
+        System.out.println("MYSQLPORT: " + System.getenv("MYSQLPORT"));
+        System.out.println("MYSQLDATABASE: " + System.getenv("MYSQLDATABASE"));
+        System.out.println("MYSQLUSER: " + System.getenv("MYSQLUSER"));
+        System.out.println("MYSQLPASSWORD: " + System.getenv("MYSQLPASSWORD"));
+    }
+}
